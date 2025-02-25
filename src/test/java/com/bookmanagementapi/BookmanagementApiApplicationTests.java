c97ce29e7a7c0829d83b601a04882be85920c124
AtansJP <atansjp2@gmail.com>
2025-02-25 00:55:59 +0000

    Initial commit: Added Spring Boot project files

---
+++ src/test/java/com/bookmanagementapi/BookmanagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
